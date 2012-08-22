1e89f72c7893a4a67b7acc47ae34147a3b274138
Ravindra Lokhande <rlokhande@nvidia.com>
2012-08-22 21:46:10 +0530

    ARM: tegra: dolak: Use correct i2c devices
    Change-Id: Idd0ff290865cfc2115056d99d36a5c16395e89ea
    
    Signed-off-by: Ravindra Lokhande <rlokhande@nvidia.com>
    Change-Id: Icd25f24937d247ae2df1b7944e56c8a2a5140693
    Reviewed-on: http://git-master/r/134099
    Reviewed-by: Bo Yan <byan@nvidia.com>

--- arch/arm/mach-tegra/board-dolak.c
+++ arch/arm/mach-tegra/board-dolak.c
@@ -221,19 +221,19 @@ static struct i2c_board_info __initdata wm8903_board_info = {
 
 static void dolak_i2c_init(void)
 {
-	tegra_i2c_device1.dev.platform_data = &dolak_i2c1_platform_data;
-	tegra_i2c_device2.dev.platform_data = &dolak_i2c2_platform_data;
-	tegra_i2c_device3.dev.platform_data = &dolak_i2c3_platform_data;
-	tegra_i2c_device4.dev.platform_data = &dolak_i2c4_platform_data;
-	tegra_i2c_device5.dev.platform_data = &dolak_i2c5_platform_data;
+	tegra11_i2c_device1.dev.platform_data = &dolak_i2c1_platform_data;
+	tegra11_i2c_device2.dev.platform_data = &dolak_i2c2_platform_data;
+	tegra11_i2c_device3.dev.platform_data = &dolak_i2c3_platform_data;
+	tegra11_i2c_device4.dev.platform_data = &dolak_i2c4_platform_data;
+	tegra11_i2c_device5.dev.platform_data = &dolak_i2c5_platform_data;
 
 	i2c_register_board_info(0, &wm8903_board_info, 1);
 
-	platform_device_register(&tegra_i2c_device5);
-	platform_device_register(&tegra_i2c_device4);
-	platform_device_register(&tegra_i2c_device3);
-	platform_device_register(&tegra_i2c_device2);
-	platform_device_register(&tegra_i2c_device1);
+	platform_device_register(&tegra11_i2c_device5);
+	platform_device_register(&tegra11_i2c_device4);
+	platform_device_register(&tegra11_i2c_device3);
+	platform_device_register(&tegra11_i2c_device2);
+	platform_device_register(&tegra11_i2c_device1);
 }
 
 #define GPIO_KEY(_id, _gpio, _iswake)		\
