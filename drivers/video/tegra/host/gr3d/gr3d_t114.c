24d1a9809ed344f947933f7f4d52e66cbc95e7c9
Terje Bergstrom <tbergstrom@nvidia.com>
2012-09-06 08:35:01 +0300

    video: tegra: host: Use actmon only if defined
    Call actmon operations only if actmon is defined and initialized for
    the SoC.
    
    Change-Id: Ia44f31757f85b54ea7486a5f6af8d8697ca915e1
    Signed-off-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Reviewed-on: http://git-master/r/129963
    (cherry picked from commit ba5cf9a001f498160447732200c966cd15e12db6)
    Reviewed-on: http://git-master/r/129913

--- drivers/video/tegra/host/gr3d/gr3d_t114.c
+++ drivers/video/tegra/host/gr3d/gr3d_t114.c
@@ -362,19 +362,22 @@ struct nvhost_hwctx_handler * __init nvhost_gr3d_t114_ctxhandler_init(
 
 void nvhost_gr3d_t114_init(struct nvhost_device *dev)
 {
-	actmon_op().init(nvhost_get_host(dev));
+	if (actmon_op().init)
+		actmon_op().init(nvhost_get_host(dev));
 	nvhost_scale3d_init(dev);
 }
 
 void nvhost_gr3d_t114_deinit(struct nvhost_device *dev)
 {
 	nvhost_scale3d_deinit(dev);
-	actmon_op().deinit(nvhost_get_host(dev));
+	if (actmon_op().deinit)
+		actmon_op().deinit(nvhost_get_host(dev));
 }
 
 int nvhost_gr3d_t114_prepare_power_off(struct nvhost_device *dev)
 {
-	actmon_op().deinit(nvhost_get_host(dev));
+	if (actmon_op().deinit)
+		actmon_op().deinit(nvhost_get_host(dev));
 	return nvhost_gr3d_prepare_power_off(dev);
 }
 
@@ -382,5 +385,6 @@ void nvhost_gr3d_t114_finalize_power_on(struct nvhost_device *dev)
 {
 	/* actmon needs to be reinitialized when we come back from
 	 * power gated state */
-	actmon_op().init(nvhost_get_host(dev));
+	if (actmon_op().init)
+		actmon_op().init(nvhost_get_host(dev));
 }
