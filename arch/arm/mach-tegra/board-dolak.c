6ed129880a643b1d4dfc7801531880f005427fc9
Chaitanya Bandi <bandik@nvidia.com>
2012-10-04 16:32:14 +0530

    ARM: tegra: dolak: Use only 1 bus for GEN2_I2C
    Use only 1 bus for GEN2_I2C instead of 2.
    
    Change-Id: I3c4dbdd3fc7907362e2402a6247511a27b9790b5
    Signed-off-by: Chaitanya Bandi <bandik@nvidia.com>
    Reviewed-on: http://git-master/r/141544
    Reviewed-by: Bo Yan <byan@nvidia.com>

--- arch/arm/mach-tegra/board-dolak.c
+++ arch/arm/mach-tegra/board-dolak.c
@@ -182,12 +182,8 @@ static const struct tegra_pingroup_config i2c2_gen2 = {
 
 static struct tegra_i2c_platform_data dolak_i2c2_platform_data = {
 	.adapter_nr	= 1,
-	.bus_count	= 2,
-	.bus_clk_rate	= { 100000, 100000 },
-#if 0	/* !!!FIXME!!!! */
-	.bus_mux	= { &i2c2_ddc, &i2c2_gen2 },
-	.bus_mux_len	= { 1, 1 },
-#endif
+	.bus_count	= 1,
+	.bus_clk_rate	= { 100000, 0 },
 };
 
 static struct tegra_i2c_platform_data dolak_i2c3_platform_data = {
