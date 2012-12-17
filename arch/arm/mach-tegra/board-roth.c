c7d53acb4be32e0575e83854a18499b522d4ead3
Suresh Mangipudi <smangipudi@nvidia.com>
2012-12-17 16:19:11 +0530

    ARM: tegra: usb_phy: Fix T11x OverCurrent selection bit
    The OCA bit in PORTSC is being set by default, this results in PORTSC
    register reporting false status for over current.
    Update the programming sequence such that the over_current bit is
    cleared.
    
    Bug 1199299
    
    Change-Id: I17b2e6c48719986a87683da9ff52ff8874d7671d
    Signed-off-by: Suresh Mangipudi <smangipudi@nvidia.com>
    Reviewed-on: http://git-master/r/171886
    (cherry picked from commit 4de2cc7c70ab9cd4f2790901b0535ffbd67f80da)
    Reviewed-on: http://git-master/r/173091
    Reviewed-by: Simone Willett <swillett@nvidia.com>
    Tested-by: Simone Willett <swillett@nvidia.com>

--- arch/arm/mach-tegra/board-roth.c
+++ arch/arm/mach-tegra/board-roth.c
@@ -483,6 +483,7 @@ static struct tegra_usb_platform_data tegra_ehci1_utmi_pdata = {
 		.xcvr_lsrslew = 2,
 		.xcvr_setup_offset = 0,
 		.xcvr_use_fuses = 1,
+		.vbus_oc_map = 0x4,
 	},
 };
 
@@ -508,6 +509,7 @@ static struct tegra_usb_platform_data tegra_ehci3_utmi_pdata = {
 		.xcvr_lsrslew = 2,
 		.xcvr_setup_offset = 0,
 		.xcvr_use_fuses = 1,
+		.vbus_oc_map = 0x5,
 	},
 };
 
