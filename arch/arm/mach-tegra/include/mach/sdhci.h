55d55afc6d722aa7a081c7a5d39c973888a4dd12
Pavan Kunapuli <pkunapuli@nvidia.com>
2013-01-22 17:48:45 +0530

    mmc: tegra: Tuning and tap selection for low freqs
    Adding frequency tuning solution for frequencies
    below 82MHz in SDR104 and HS200 mode.
    
    Bug 1189241
    Bug 1181574
    
    Reviewed-on: http://git-master/r/192114
    (cherry picked from commit 39cf979cd0a55876b8ceeaa75abbc19dd6527051)
    Change-Id: Iec55f36de850060c71a13b5dd42d815e573c1f1b
    Signed-off-by: Pavan Kunapuli <pkunapuli@nvidia.com>
    Reviewed-on: http://git-master/r/212421
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Venu Byravarasu <vbyravarasu@nvidia.com>

--- arch/arm/mach-tegra/include/mach/sdhci.h
+++ arch/arm/mach-tegra/include/mach/sdhci.h
@@ -4,7 +4,7 @@
  * Copyright (C) 2009 Palm, Inc.
  * Author: Yvonne Yip <y@palm.com>
  *
- * Copyright (c) 2012-13, NVIDIA CORPORATION.
+ * Copyright (c) 2013, NVIDIA CORPORATION.  All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -45,6 +45,7 @@ struct tegra_sdhci_platform_data {
 	int is_8bit;
 	int pm_flags;
 	int pm_caps;
+	int nominal_vcore_uV;
 	unsigned int max_clk_limit;
 	unsigned int ddr_clk_limit;
 	unsigned int tap_delay;
