5cf8a066e360c35222bc82cc706539026aa5b764
Laxman Dewangan <ldewangan@nvidia.com>
2013-02-07 17:33:00 +0530

    ARM: tegra: ceres: remove dead code
    Remove commented code to make the file simple.
    
    Change-Id: Ic6f652245b54dd8e80bbdaa609b7bb011816ecc5
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: http://git-master/r/198354

--- arch/arm/mach-tegra/board-ceres-kbc.c
+++ arch/arm/mach-tegra/board-ceres-kbc.c
@@ -24,7 +24,6 @@
 #include <linux/input.h>
 #include <mach/io.h>
 #include <mach/iomap.h>
-#include <mach/kbc.h>
 #include <linux/gpio.h>
 #include <linux/gpio_keys.h>
 #include <linux/mfd/palmas.h>
@@ -34,57 +33,6 @@
 #include "board-ceres.h"
 #include "devices.h"
 
-/*
-static const u32 kbd_keymap[] = {
-	KEY(0, 0, KEY_POWER),
-	KEY(0, 1, KEY_HOME),
-
-	KEY(1, 0, KEY_RESERVED),
-	KEY(1, 1, KEY_VOLUMEDOWN),
-
-	KEY(2, 0, KEY_CAMERA),
-	KEY(2, 1, KEY_VOLUMEUP),
-	KEY(2, 2, KEY_2),
-};
-
-static const struct matrix_keymap_data keymap_data = {
-	.keymap		= kbd_keymap,
-	.keymap_size	= ARRAY_SIZE(kbd_keymap),
-};
-
-static struct tegra_kbc_wake_key ceres_wake_cfg[] = {
-	[0] = {
-		.row = 0,
-		.col = 0,
-	},
-};
-
-static struct tegra_kbc_platform_data ceres_kbc_platform_data = {
-	.debounce_cnt = 20 * 32,
-	.repeat_cnt = 1,
-	.scan_count = 30,
-	.wakeup = true,
-	.keymap_data = &keymap_data,
-	.wake_cnt = 2,
-	.wake_cfg = &ceres_wake_cfg[0],
-	.wakeup_key = KEY_POWER,
-#ifdef CONFIG_ANDROID
-	.disable_ev_rep = true,
-#endif
-};
-*/
-/*
-#define GPIO_IKEY(_id, _irq, _iswake, _deb)	\
-	{					\
-		.code = _id,			\
-		.gpio = -1,			\
-		.irq = _irq,			\
-		.desc = #_id,			\
-		.type = EV_KEY,			\
-		.wakeup = _iswake,		\
-		.debounce_interval = _deb,	\
-	}
-*/
 #define GPIO_KEY(_id, _gpio, _iswake)		\
 	{					\
 		.code = _id,			\
