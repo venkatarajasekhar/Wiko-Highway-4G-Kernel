65ffb00ad19527375b226fe7b565e8efb1713a46
Laxman Dewangan <ldewangan@nvidia.com>
2013-02-10 00:44:58 +0530

    mfd: palmas: read internal design rev and sw otp revsion of device
    Read internal design revision and sw otp version of the device and
    print this message.
    
    This will help on implementing the errata of different ES version of
    device.
    
    bug 1228386
    bug 1195226
    
    (Cherrypicked from commit
    	4aae1b8da409ec83b0edff1e7d9d34bdb4b5ede9)
    
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: http://git-master/r/199131
    
    Change-Id: I616aafd20cc0fce0f75a2f730107905f739f77cb
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: http://git-master/r/215541
    Reviewed-by: Automatic_Commit_Validation_User

--- include/linux/mfd/palmas.h
+++ include/linux/mfd/palmas.h
@@ -50,6 +50,11 @@ struct palmas {
 	u16 gpio_muxed;
 	u8 led_muxed;
 	u8 pwm_muxed;
+
+	int design_revision;
+	int sw_otp_version;
+	int es_minor_version;
+	int es_major_version;
 };
 
 struct palmas_reg_init {
@@ -359,6 +364,7 @@ struct palmas_pmic {
 #define PALMAS_USB_BASE						0x290
 #define PALMAS_GPADC_BASE					0x2C0
 #define PALMAS_TRIM_GPADC_BASE					0x3CD
+#define PALMAS_PAGE3_BASE					0x300
 
 #define PALMAS_CHARGE_PUMP_CTRL					0x7C
 /* Bit definitions for CHARGE_PUMP_CTRL */
@@ -3195,6 +3201,9 @@ struct palmas_pmic {
 #define PALMAS_GPADC_SMPS_VSEL_MONITORING_ACTIVE_PHASE_SHIFT	7
 #define PALMAS_GPADC_SMPS_VSEL_MONITORING_SMPS_VSEL_MONITORING_MASK	0x7f
 #define PALMAS_GPADC_SMPS_VSEL_MONITORING_SMPS_VSEL_MONITORING_SHIFT	0
+
+#define PALMAS_INTERNAL_DESIGNREV				0x57
+#define PALMAS_INTERNAL_DESIGNREV_DESIGNREV(val)		((val) & 0xF)
 
 /* Registers for function GPADC */
 #define PALMAS_GPADC_TRIM1					0x0
