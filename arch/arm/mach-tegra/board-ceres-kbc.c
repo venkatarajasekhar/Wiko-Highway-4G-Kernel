2780060fd52df507ef8639b8da01038e3aacfb74
Laxman Dewangan <ldewangan@nvidia.com>
2013-02-11 19:20:01 +0530

    ARM: tegra: ceres: swap the vol-up/dn pin number
    bug 1233687
    
    Change-Id: I67399756a6a55cb4ed1dfb069a8312284f4d2ba4
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: http://git-master/r/199441

--- arch/arm/mach-tegra/board-ceres-kbc.c
+++ arch/arm/mach-tegra/board-ceres-kbc.c
@@ -72,8 +72,8 @@ static struct gpio_keys_button ceres_int_keys[] = {
 	[1] = GPIO_KEY(KEY_MENU, PJ2, 0),
 	[2] = GPIO_KEY(KEY_BACK, PJ3, 0),
 	[3] = GPIO_IKEY(KEY_POWER, CERES_POWER_ON_INT, 1, 100),
-	[4] = GPIO_KEY(KEY_VOLUMEUP, PJ5, 0),
-	[5] = GPIO_KEY(KEY_VOLUMEDOWN, PJ6, 0),
+	[4] = GPIO_KEY(KEY_VOLUMEUP, PJ6, 0),
+	[5] = GPIO_KEY(KEY_VOLUMEDOWN, PJ5, 0),
 	[6] = GPIO_IKEY(KEY_POWER, CERES_POWER_LONGPRESS_INT, 0, 1000),
 
 #endif
