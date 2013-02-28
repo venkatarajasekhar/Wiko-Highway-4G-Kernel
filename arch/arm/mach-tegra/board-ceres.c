5a3571a6f8425134c4a895806593acd0cfa7b8c2
Chaitanya Bandi <bandik@nvidia.com>
2013-02-28 11:46:08 +0530

    ARM: tegra: ceres: Remove registration of tegra-rtc
    Removed registration of tegra-rtc in ceres as the
    primary rtc device is max77660
    
    Bug 1241751
    
    Change-Id: I4e5675b015423fcb6b9c7e5210f3cf6adff8a76a
    Signed-off-by: Chaitanya Bandi <bandik@nvidia.com>
    Reviewed-on: http://git-master/r/204975
    (cherry picked from commit 94caa7c85e1b7b8728b066c0cc3de4e2aa4d7208)
    Reviewed-on: http://git-master/r/206946
    Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>
    Tested-by: Bitan Biswas <bbiswas@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User

--- arch/arm/mach-tegra/board-ceres.c
+++ arch/arm/mach-tegra/board-ceres.c
@@ -186,26 +186,6 @@ static struct i2c_board_info __initdata ceres_i2c_bus3_board_info[] = {
 	},
 };
 
-static struct resource tegra_rtc_resources[] = {
-	[0] = {
-		.start = TEGRA_RTC_BASE,
-		.end = TEGRA_RTC_BASE + TEGRA_RTC_SIZE - 1,
-		.flags = IORESOURCE_MEM,
-	},
-	[1] = {
-		.start = INT_RTC,
-		.end = INT_RTC,
-		.flags = IORESOURCE_IRQ,
-	},
-};
-
-static struct platform_device tegra_rtc_device = {
-	.name = "tegra_rtc",
-	.id   = -1,
-	.resource = tegra_rtc_resources,
-	.num_resources = ARRAY_SIZE(tegra_rtc_resources),
-};
-
 static struct max98090_eq_cfg max98090_eq_cfg[] = {
 };
 
@@ -396,7 +376,6 @@ static struct platform_device *ceres_audio_devices[] __initdata = {
 
 static struct platform_device *ceres_devices[] __initdata = {
 	&tegra_pmu_device,
-	&tegra_rtc_device,
 	&tegra_udc_device,
 #if defined(CONFIG_TEGRA_AVP)
 	&tegra_avp_device,
