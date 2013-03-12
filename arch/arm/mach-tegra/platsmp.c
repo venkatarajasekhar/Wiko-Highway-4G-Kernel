a3ab2feb610c68b0efa046bf1391cb081cf02e5f
Mayuresh Kulkarni <mkulkarni@nvidia.com>
2013-03-12 15:39:43 +0530

    ARM: tegra: remove duplicate defines for chip_id
    - the chip id have been defined in 2 places: fuse.h as
    TEGRAXX & mach/hardware.h as TEGRA_CHIPID_TEGRAXX
    - some part of code used defines from fuse.h while others
    used mach/hardware.h
    - this commit removes the defines in fuse.h and converts
    all the code to use the defines from mach/hardware.h
    - kernel in dev-chips is also using defines from
    mach/hardware.h
    
    Change-Id: Ia65f8ead3f8eccdd6cba4a159c3d0e16a2566e41
    Signed-off-by: Mayuresh Kulkarni <mkulkarni@nvidia.com>
    Reviewed-on: http://git-master/r/208375
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- arch/arm/mach-tegra/platsmp.c
+++ arch/arm/mach-tegra/platsmp.c
@@ -337,7 +337,7 @@ int tegra_boot_secondary(unsigned int cpu, struct task_struct *idle)
 #endif
 
 	switch (tegra_chip_id) {
-	case TEGRA20:
+	case TEGRA_CHIPID_TEGRA2:
 		/*
 		 * Unhalt the CPU. If the flow controller was used to power-gate
 		 * the CPU this will cause the flow controller to stop driving
@@ -347,7 +347,7 @@ int tegra_boot_secondary(unsigned int cpu, struct task_struct *idle)
 		flowctrl_write_cpu_halt(cpu, 0);
 		status = tegra20_power_up_cpu(cpu);
 		break;
-	case TEGRA30:
+	case TEGRA_CHIPID_TEGRA3:
 		/*
 		 * Unhalt the CPU. If the flow controller was used to power-gate
 		 * the CPU this will cause the flow controller to stop driving
@@ -357,8 +357,8 @@ int tegra_boot_secondary(unsigned int cpu, struct task_struct *idle)
 		flowctrl_write_cpu_halt(cpu, 0);
 		status = tegra30_power_up_cpu(cpu);
 		break;
-	case TEGRA11X:
-	case TEGRA14X:
+	case TEGRA_CHIPID_TEGRA11:
+	case TEGRA_CHIPID_TEGRA14:
 		status = tegra11x_power_up_cpu(cpu);
 		break;
 	default:
