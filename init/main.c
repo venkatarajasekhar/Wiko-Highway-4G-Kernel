3fddb28c0484743e3efe6b24ee70868b8d96cdd3
Maxime Coquelin <maxime.coquelin@stericsson.com>
2013-04-04 11:17:03 +0530

    PASR: Call PASR initialization
    Bug 1201663
    Bug 1033159
    
    Change-Id: Ie4a9cfd6632c40a91fed15fd617c0982a130a0af
    Signed-off-by: Maxime Coquelin <maxime.coquelin@stericsson.com>
    Signed-off-by: Prashant Gaikwad <pgaikwad@nvidia.com>
    Reviewed-on: http://git-master/r/216387
    Reviewed-by: Simone Willett <swillett@nvidia.com>
    Tested-by: Simone Willett <swillett@nvidia.com>

--- init/main.c
+++ init/main.c
@@ -69,6 +69,7 @@
 #include <linux/slab.h>
 #include <linux/perf_event.h>
 
+#include <linux/pasr.h>
 #include <asm/io.h>
 #include <asm/bugs.h>
 #include <asm/setup.h>
@@ -494,6 +495,9 @@ asmlinkage void __init start_kernel(void)
 	page_address_init();
 	printk(KERN_NOTICE "%s", linux_banner);
 	setup_arch(&command_line);
+#ifdef CONFIG_PASR
+	early_pasr_setup();
+#endif
 	mm_init_owner(&init_mm, &init_task);
 	mm_init_cpumask(&init_mm);
 	setup_command_line(command_line);
@@ -561,6 +565,10 @@ asmlinkage void __init start_kernel(void)
 	local_irq_enable();
 
 	kmem_cache_init_late();
+
+#ifdef CONFIG_PASR
+	late_pasr_setup();
+#endif
 
 	/*
 	 * HACK ALERT! This is early. We're enabling the console before
