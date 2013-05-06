2c15644a837839c0bfb5e205ad89c861099cf426
Sivaram Nair <sivaramn@nvidia.com>
2013-05-06 11:21:55 +0300

    ARM: tegra: pluto: restructuring system EDP code
    Moving system EDP functions to board-pluto-power.c
    
    Bug 1274376
    
    Change-Id: I38aa90420026e89be8f8bb92f4f4bbc26a211c5f
    Signed-off-by: Sivaram Nair <sivaramn@nvidia.com>
    Reviewed-on: http://git-master/r/226224
    (cherry picked from commit 74683030f75653a9263e9bbcf9c03d231445a823)
    Reviewed-on: http://git-master/r/226664
    Reviewed-by: Mrutyunjay Sawant <msawant@nvidia.com>
    Tested-by: Mrutyunjay Sawant <msawant@nvidia.com>

--- arch/arm/mach-tegra/board-pluto.c
+++ arch/arm/mach-tegra/board-pluto.c
@@ -50,7 +50,6 @@
 #include <linux/mfd/max8831.h>
 #include <linux/of_platform.h>
 #include <linux/a2220.h>
-#include <linux/edp.h>
 #include <linux/mfd/tlv320aic3262-registers.h>
 #include <linux/mfd/tlv320aic3xxx-core.h>
 
@@ -1357,42 +1356,6 @@ static int __init pluto_touch_init(void)
 	return 0;
 }
 
-#ifdef CONFIG_EDP_FRAMEWORK
-static struct edp_manager battery_edp_manager = {
-	.name = "battery",
-	.max = 14000
-};
-
-static void __init pluto_battery_edp_init(void)
-{
-	struct edp_governor *g;
-	int r;
-
-	r = edp_register_manager(&battery_edp_manager);
-	if (r)
-		goto err_ret;
-
-	/* start with priority governor */
-	g = edp_get_governor("priority");
-	if (!g) {
-		r = -EFAULT;
-		goto err_ret;
-	}
-
-	r = edp_set_governor(&battery_edp_manager, g);
-	if (r)
-		goto err_ret;
-
-	return;
-
-err_ret:
-	pr_err("Battery EDP init failed with error %d\n", r);
-	WARN_ON(1);
-}
-#else
-static inline void pluto_battery_edp_init(void) {}
-#endif
-
 #ifdef CONFIG_USE_OF
 struct of_dev_auxdata pluto_auxdata_lookup[] __initdata = {
 	OF_DEV_AUXDATA("nvidia,tegra114-sdhci", 0x78000600, "sdhci-tegra.3",
@@ -1430,7 +1393,7 @@ static void __init pluto_dtv_init(void)
 
 static void __init tegra_pluto_early_init(void)
 {
-	pluto_battery_edp_init();
+	pluto_sysedp_init();
 	tegra_clk_init_from_table(pluto_clk_init_table);
 	tegra_clk_verify_parents();
 	tegra_soc_device_init("tegra_pluto");
@@ -1475,6 +1438,7 @@ static void __init tegra_pluto_late_init(void)
 	tegra_serial_debug_init(TEGRA_UARTD_BASE, INT_WDT_CPU, NULL, -1, -1);
 	pluto_soctherm_init();
 	tegra_register_fuse();
+	pluto_sysedp_core_init();
 }
 
 static void __init pluto_ramconsole_reserve(unsigned long size)
