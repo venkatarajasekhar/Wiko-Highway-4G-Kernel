7214e032382d8c15744217f178d91aff8f3746db
Ajay Gupta <ajayg@nvidia.com>
2013-05-14 12:09:59 -0700

    ARM: tegra: refactor xusb registration
    Refactored xusb device registration to support multi chip
    and multi board configuration.
    
    Added arch/arm/mach-tegra/xusb.c where all board files belonging
    to any tegra chip would call tegra_xusb_init() with board specific
    config to register xusb platform device.
    
    Bug 1268244
    
    Change-Id: I6fc08e9c762e93c68172be58b187add6f62171cb
    Signed-off-by: Ajay Gupta <ajayg@nvidia.com>
    Reviewed-on: http://git-master/r/228499
    Reviewed-on: http://git-master/r/235050
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Ashutosh Jha <ajha@nvidia.com>

--- arch/arm/mach-tegra/board-pluto.c
+++ arch/arm/mach-tegra/board-pluto.c
@@ -75,6 +75,7 @@
 #include <mach/tegra_wakeup_monitor.h>
 #include <linux/platform_data/tegra_usb_modem_power.h>
 #include <mach/hardware.h>
+#include <mach/xusb.h>
 
 #include "board.h"
 #include "board-common.h"
@@ -1153,6 +1154,13 @@ static struct tegra_xusb_pad_data xusb_padctl_data = {
 	.hsic_pad0_ctl1 = (0x00 << 8),
 };
 
+static struct tegra_xusb_board_data xusb_bdata = {
+	.padctl_data = &xusb_padctl_data,
+	.portmap = TEGRA_XUSB_SS_P0 | TEGRA_XUSB_USB2_P0,
+	/* ss_portmap[0:3] = SS0 map, ss_portmap[4:7] = SS1 map */
+	.ss_portmap = (TEGRA_XUSB_SS_PORT_MAP_USB2_P0 << 0),
+};
+
 static void pluto_xusb_init(void)
 {
 	int usb_port_owner_info = tegra_get_usb_port_owner_info();
@@ -1176,8 +1184,7 @@ static void pluto_xusb_init(void)
 		xusb_padctl_data.hs_iref_cap = (usb_calib0 >> 13) & 0x3;
 		xusb_padctl_data.hs_curr_level_pad1 = (usb_calib0 >> 15) & 0x3f;
 
-		tegra_xhci_device.dev.platform_data = &xusb_padctl_data;
-		platform_device_register(&tegra_xhci_device);
+		tegra_xusb_init(&xusb_bdata);
 	}
 }
 #else
