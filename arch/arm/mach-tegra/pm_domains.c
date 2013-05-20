60248f263cea85d4be32baf6093e9acfa0f9e543
Mayuresh Kulkarni <mkulkarni@nvidia.com>
2013-05-20 17:49:45 +0530

    ARM: tegra: enable MC clock domain
    bug 1272223
    
    Change-Id: I83cf72f99107f7240deec709fb5596ec1cc13424
    Signed-off-by: Mayuresh Kulkarni <mkulkarni@nvidia.com>
    Reviewed-on: http://git-master/r/232579
    Reviewed-by: Prashant Gaikwad <pgaikwad@nvidia.com>
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- arch/arm/mach-tegra/pm_domains.c
+++ arch/arm/mach-tegra/pm_domains.c
@@ -238,9 +238,11 @@ static int __init tegra_init_pm_domain(void)
 	pm_genpd_init(&tegra_mc_clk.gpd, &simple_qos_governor, false);
 
 #ifdef CONFIG_ARCH_TEGRA_14x_SOC
+	tegra_mc_chain_a.clk = clk_get_sys("mc_capa", "mc_capa");
 	pm_genpd_init(&tegra_mc_chain_a.gpd, &simple_qos_governor, false);
 	tegra_pd_add_sd(&tegra_mc_chain_a.gpd);
 
+	tegra_mc_chain_b.clk = clk_get_sys("mc_cbpa", "mc_cbpa");
 	pm_genpd_init(&tegra_mc_chain_b.gpd, &simple_qos_governor, false);
 	tegra_pd_add_sd(&tegra_mc_chain_b.gpd);
 #endif
