8f02ca8327140b8df68930ad9e78bfd25c564813
Hyungwoo Yang <hyungwooy@nvidia.com>
2013-05-22 15:40:46 -0700

    ARM: tegra: ceres: Optimize thermal margins for ceres
    modified thermal margins for ceres.
    
    Bug 1274039
    
    Change-Id: Id67ffe907232404dc4ce3132df804baa044b24f1
    Signed-off-by: Hyungwoo Yang <hyungwooy@nvidia.com>
    Reviewed-on: http://git-master/r/231786
    Reviewed-by: Mrutyunjay Sawant <msawant@nvidia.com>
    Tested-by: Mrutyunjay Sawant <msawant@nvidia.com>

--- arch/arm/mach-tegra/board-ceres-power.c
+++ arch/arm/mach-tegra/board-ceres-power.c
@@ -1094,21 +1094,21 @@ static struct soctherm_platform_data ceres_soctherm_data = {
 			.trips = {
 				{
 					.cdev_type = "tegra-balanced",
-					.trip_temp = 90000,
+					.trip_temp = 86000,
 					.trip_type = THERMAL_TRIP_PASSIVE,
 					.upper = THERMAL_NO_LIMIT,
 					.lower = THERMAL_NO_LIMIT,
 				},
 				{
 					.cdev_type = "tegra-heavy",
-					.trip_temp = 100000,
+					.trip_temp = 96000,
 					.trip_type = THERMAL_TRIP_HOT,
 					.upper = THERMAL_NO_LIMIT,
 					.lower = THERMAL_NO_LIMIT,
 				},
 				{
 					.cdev_type = "tegra-shutdown",
-					.trip_temp = 102000,
+					.trip_temp = 98000,
 					.trip_type = THERMAL_TRIP_CRITICAL,
 					.upper = THERMAL_NO_LIMIT,
 					.lower = THERMAL_NO_LIMIT,
@@ -1124,21 +1124,21 @@ static struct soctherm_platform_data ceres_soctherm_data = {
 			.trips = {
 				{
 					.cdev_type = "tegra-balanced",
-					.trip_temp = 90000,
+					.trip_temp = 88000,
 					.trip_type = THERMAL_TRIP_PASSIVE,
 					.upper = THERMAL_NO_LIMIT,
 					.lower = THERMAL_NO_LIMIT,
 				},
 				{
 					.cdev_type = "tegra-heavy",
-					.trip_temp = 100000,
+					.trip_temp = 98000,
 					.trip_type = THERMAL_TRIP_HOT,
 					.upper = THERMAL_NO_LIMIT,
 					.lower = THERMAL_NO_LIMIT,
 				},
 				{
 					.cdev_type = "tegra-shutdown",
-					.trip_temp = 102000,
+					.trip_temp = 100000,
 					.trip_type = THERMAL_TRIP_CRITICAL,
 					.upper = THERMAL_NO_LIMIT,
 					.lower = THERMAL_NO_LIMIT,
