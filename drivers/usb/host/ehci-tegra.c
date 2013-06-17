c79bafd2665de8274556d85e0f722351e9b0efbd
Rakesh Bodla <rbodla@nvidia.com>
2013-06-17 20:55:42 +0530

    usb: host: tegra: Fix pm qos procedure calls
    During driver unregistration pm_remove_request
    should be called remove the element from pm qos
    list.
    
    Bug 1307563
    
    Change-Id: Iaf5410bb6c606722a04a15fd321c2b58a91f0feb
    Signed-off-by: Rakesh Bodla <rbodla@nvidia.com>
    Reviewed-on: http://git-master/r/239337
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Venkat Moganty <vmoganty@nvidia.com>

--- drivers/usb/host/ehci-tegra.c
+++ drivers/usb/host/ehci-tegra.c
@@ -694,8 +694,7 @@ static int tegra_ehci_remove(struct platform_device *pdev)
 
 #ifdef CONFIG_TEGRA_EHCI_BOOST_CPU_FREQ
 	cancel_delayed_work(&tegra->boost_cpu_freq_work);
-	pm_qos_update_request(&tegra->boost_cpu_freq_req,
-				PM_QOS_DEFAULT_VALUE);
+	pm_qos_remove_request(&tegra->boost_cpu_freq_req);
 	tegra->cpu_boost_in_work = false;
 #endif
 	rhdev = hcd->self.root_hub;
