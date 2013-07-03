a0163ae1be4f13bd7a4fe5d0db9bd1ef53cf40d5
Krishna Reddy <vdumpa@nvidia.com>
2013-07-03 10:05:45 -0700

    arm: mm: disable write combining for dma prot
    Write combining prot is not coherent on CPU_v7 with outer cache.
    Use uncache memory prot when outer cache is enabled.
    
    Change-Id: I5eb118957cfba5e53a007a01a218eb6def9f6047
    Signed-off-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-on: http://git-master/r/244802
    Reviewed-by: Mrutyunjay Sawant <msawant@nvidia.com>
    Tested-by: Mrutyunjay Sawant <msawant@nvidia.com>

--- arch/arm/include/asm/pgtable.h
+++ arch/arm/include/asm/pgtable.h
@@ -114,7 +114,7 @@ extern pgprot_t		pgprot_kernel;
 #define pgprot_stronglyordered(prot) \
 	__pgprot_modify(prot, L_PTE_MT_MASK, L_PTE_MT_UNCACHED)
 
-#ifdef CONFIG_ARM_DMA_MEM_BUFFERABLE
+#if defined(CONFIG_ARM_DMA_MEM_BUFFERABLE) && !defined(CONFIG_OUTER_CACHE)
 #define pgprot_dmacoherent(prot) \
 	__pgprot_modify(prot, L_PTE_MT_MASK, L_PTE_MT_BUFFERABLE | L_PTE_XN)
 #define __HAVE_PHYS_MEM_ACCESS_PROT
