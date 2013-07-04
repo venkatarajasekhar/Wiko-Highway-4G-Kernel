06f8722ba9a8288c446f01709b7693140d01eb8e
Sivaram Nair <sivaramn@nvidia.com>
2013-07-04 14:22:21 +0300

    ARM: tegra: bbc: reorder EDP client registration
    bbc_boot client registration is moved before bbc client registration.
    Sys EDP manager does not have enough budget to support both at the same
    time, which means that the bbc client registation will fail (E0 sum >=
    battery cap).
    
    Bug 1269041
    
    Change-Id: I7b0a9b90bcd155fd8e0e601421ec804ea2c44f0a
    Signed-off-by: Sivaram Nair <sivaramn@nvidia.com>
    Reviewed-on: http://git-master/r/245109
    Reviewed-by: Mrutyunjay Sawant <msawant@nvidia.com>
    Tested-by: Mrutyunjay Sawant <msawant@nvidia.com>

--- arch/arm/mach-tegra/tegra_bbc_proxy.c
+++ arch/arm/mach-tegra/tegra_bbc_proxy.c
@@ -164,14 +164,6 @@ int tegra_bbc_proxy_edp_register(struct device *dev, u32 num_states,
 		goto done;
 	}
 
-	/* register modem client */
-	ret = edp_register_client(mgr, &bbc->modem_edp_client);
-	if (ret) {
-		dev_err(dev, "unable to register bbc edp client\n");
-		goto done;
-	}
-	bbc->edp_client_registered = 1;
-
 	/* unregister modem_boot_client */
 	ret = edp_unregister_client(bbc->modem_boot_edp_client);
 	if (ret) {
@@ -180,6 +172,15 @@ int tegra_bbc_proxy_edp_register(struct device *dev, u32 num_states,
 	}
 
 	bbc->edp_boot_client_registered = 0;
+
+	/* register modem client */
+	ret = edp_register_client(mgr, &bbc->modem_edp_client);
+	if (ret) {
+		dev_err(dev, "unable to register bbc edp client\n");
+		goto done;
+	}
+
+	bbc->edp_client_registered = 1;
 
 	ret = edp_update_client_request(&bbc->modem_edp_client, 0, NULL);
 	if (ret) {
