29ea2ad55d32eeade9e67e039727dc8436e7444c
Sivaram Nair <sivaramn@nvidia.com>
2013-07-11 17:14:04 +0300

    ARM: tegra14: Ceres: enable modem OC throttling
    Add necessary board data for modem OC throttling. This patch also
    applies to Atlantis.
    
    Bug 1313669
    
    Change-Id: I7ff64c06b2521f80db0b5dcae46baa1745460a1c
    Signed-off-by: Sivaram Nair <sivaramn@nvidia.com>
    Reviewed-on: http://git-master/r/248231
    Reviewed-by: Mrutyunjay Sawant <msawant@nvidia.com>
    Tested-by: Mrutyunjay Sawant <msawant@nvidia.com>

--- arch/arm/mach-tegra/board-ceres-power.c
+++ arch/arm/mach-tegra/board-ceres-power.c
@@ -1385,6 +1385,19 @@ static struct soctherm_platform_data ceres_soctherm_data = {
 				},
 			},
 		},
+		[THROTTLE_OC2] = {
+			.throt_mode = BRIEF,
+			.devs = {
+				[THROTTLE_DEV_CPU] = {
+					.enable = true,
+					.depth = 50,
+				},
+				[THROTTLE_DEV_GPU] = {
+					.enable = true,
+					.depth = 50,
+				},
+			},
+		},
 	},
 	.tshut_pmu_trip_data = &tpdata_max77660,
 };
