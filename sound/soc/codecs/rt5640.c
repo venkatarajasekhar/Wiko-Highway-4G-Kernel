a1e46b07494c30b06997c997861c72a2d8e5bab0
Philip Rakity <prakity@nvidia.com>
2013-07-22 10:50:08 +0100

    asoc: codecs: rt5640.c  Fix warnings.
    fix possible unitialized variable warning
    
    Change-Id: I13a9ee882af139f3b7decdc2f26fcc60b5d46c79
    Signed-off-by: Philip Rakity <prakity@nvidia.com>
    Reviewed-on: http://git-master/r/251879
    Reviewed-by: Mrutyunjay Sawant <msawant@nvidia.com>
    Tested-by: Mrutyunjay Sawant <msawant@nvidia.com>

--- sound/soc/codecs/rt5640.c
+++ sound/soc/codecs/rt5640.c
@@ -2460,7 +2460,8 @@ static int rt5640_pll_calc(const unsigned int freq_in,
 	const unsigned int freq_out, struct rt5640_pll_code *pll_code)
 {
 	int max_n = RT5640_PLL_N_MAX, max_m = RT5640_PLL_M_MAX;
-	int n, m, red, n_t, m_t, in_t, out_t, red_t = abs(freq_out - freq_in);
+	int red, n_t, m_t, in_t, out_t, red_t = abs(freq_out - freq_in);
+	int n = 0, m = 0;
 	bool bypass = false;
 
 	if (RT5640_PLL_INP_MAX < freq_in || RT5640_PLL_INP_MIN > freq_in)
