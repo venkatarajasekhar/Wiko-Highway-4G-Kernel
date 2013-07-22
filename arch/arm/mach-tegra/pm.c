6593380d81a67baa32c3f3019eddfff429100037
Seshendra Gadagottu <sgadagottu@nvidia.com>
2013-07-22 11:41:30 -0700

    ARM: tegra14: pm: Fix mods build issue
    Fixing compilation issue related to unused variables
    in mods build by keeping them under CONFIG_CRYPTO_DEV_TEGRA_SE
    switch.
    
    Bug 1325315
    
    Change-Id: I823e4e1caf991891dfe72a51fc0b2da9e0287acf
    Signed-off-by: Seshendra Gadagottu <sgadagottu@nvidia.com>
    Reviewed-on: http://git-master/r/252039
    Reviewed-by: Yu-Huan Hsu <yhsu@nvidia.com>
    Reviewed-by: Kiran Kasamsetty <kkasamsetty@nvidia.com>
    Reviewed-by: Bo Yan <byan@nvidia.com>

--- arch/arm/mach-tegra/pm.c
+++ arch/arm/mach-tegra/pm.c
@@ -1093,7 +1093,7 @@ static int tegra_suspend_enter(suspend_state_t state)
 	ktime_t delta;
 	struct timespec ts_entry, ts_exit;
 
-#if defined(CONFIG_ARCH_TEGRA_14x_SOC)
+#if defined(CONFIG_CRYPTO_DEV_TEGRA_SE) && defined(CONFIG_ARCH_TEGRA_14x_SOC)
 	u32 suspend_state;
 	u32 reg;
 #endif
