2094c2441352b759c59ca962a0d574ce60b94fe2
Darbha Sriharsha <dsriharsha@nvidia.com>
2013-08-13 15:12:57 +0530

    power: max17048: Kernel warning on low battery
    Print kernel warning logs when battery is running
    low. Print messgaes at soc levels of 15%, 10% and 5%
    
    Bug 1216617
    
    Change-Id: Ia2513c9d654dc7a077f6ef10c3d0c92c26b39823
    Signed-off-by: Darbha Sriharsha <dsriharsha@nvidia.com>
    Reviewed-on: http://git-master/r/261000
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/power/max17048_battery.c
+++ drivers/power/max17048_battery.c
@@ -185,6 +185,15 @@ static int max17048_get_property(struct power_supply *psy,
 		break;
 	case POWER_SUPPLY_PROP_CAPACITY:
 		val->intval = chip->soc;
+		if (chip->soc == 15)
+			dev_warn(&chip->client->dev,
+			"\nSystem Running low on battery - 15%\n");
+		if (chip->soc == 10)
+			dev_warn(&chip->client->dev,
+			"\nSystem Running low on battery - 10%\n");
+		if (chip->soc == 5)
+			dev_warn(&chip->client->dev,
+			"\nSystem Running low on battery - 5%\n");
 		break;
 	case POWER_SUPPLY_PROP_HEALTH:
 		val->intval = chip->health;
