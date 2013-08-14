cd44e0e9af34ee01900d61f2a8bc995644090cb6
Philip Rakity <prakity@nvidia.com>
2013-08-14 10:04:18 +0100

    power: max17048: fix /n to \n print conversion
    /n is the escape for newline
    
    Change-Id: I7e43e86934ded867cd7bb6e78f167636bad83e39
    Signed-off-by: Philip Rakity <prakity@nvidia.com>
    Reviewed-on: http://git-master/r/261446
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Darbha Sriharsha <dsriharsha@nvidia.com>
    Tested-by: Darbha Sriharsha <dsriharsha@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/power/max17048_battery.c
+++ drivers/power/max17048_battery.c
@@ -187,13 +187,13 @@ static int max17048_get_property(struct power_supply *psy,
 		val->intval = chip->soc;
 		if (chip->soc == 15)
 			dev_warn(&chip->client->dev,
-			"\nSystem Running low on battery - 15%\n");
+			"/nSystem Running low on battery - 15%\n");
 		if (chip->soc == 10)
 			dev_warn(&chip->client->dev,
-			"\nSystem Running low on battery - 10%\n");
+			"/nSystem Running low on battery - 10%\n");
 		if (chip->soc == 5)
 			dev_warn(&chip->client->dev,
-			"\nSystem Running low on battery - 5%\n");
+			"/nSystem Running low on battery - 5%\n");
 		break;
 	case POWER_SUPPLY_PROP_HEALTH:
 		val->intval = chip->health;
