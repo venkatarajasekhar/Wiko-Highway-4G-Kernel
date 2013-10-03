e0b516ebf18aadcaf378bd093f121a73b3b66562
Om Prakash Singh <omp@nvidia.com>
2013-10-03 13:26:00 +0530

    net: wireless: bcmdhd: use only 20MHz for P2P GO
    As BCM4335 and other 802.11ac chip supports 80MHz connection, default
    P2P connection happens on 80MHz. It is observed that connection in 80MHz
    consumes more power than 20MHz connection. There is 70mW power
    difference between connection in 20Mhz and 80MHz. Limiting P2P connection
    to 20MHz for power optimization in Miracast usecase.
    
    Bug 1331026
    
    Change-Id: Ib617a852880f5168171103aab4e6e419cd1d4a22
    Signed-off-by: Om Prakash Singh <omp@nvidia.com>
    Reviewed-on: http://git-master/r/281325
    Reviewed-by: Narayan Reddy <narayanr@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- drivers/net/wireless/bcmdhd/wl_cfg80211.c
+++ drivers/net/wireless/bcmdhd/wl_cfg80211.c
@@ -4666,11 +4666,13 @@ wl_cfg80211_set_channel(struct wiphy *wiphy, struct net_device *dev,
 	u32 bw = WL_CHANSPEC_BW_20;
 
 	s32 err = BCME_OK;
+#ifndef P2P_GO_CHANSPEC_BW_20
 	s32 bw_cap = 0;
 	struct {
 		u32 band;
 		u32 bw_cap;
 	} param = {0, 0};
+#endif
 	struct wl_priv *wl = wiphy_priv(wiphy);
 
 	dev = ndev_to_wlc_ndev(dev, wl);
@@ -4678,7 +4680,7 @@ wl_cfg80211_set_channel(struct wiphy *wiphy, struct net_device *dev,
 	WL_ERR(("netdev_ifidx(%d), chan_type(%d) target channel(%d) \n",
 		dev->ifindex, channel_type, _chan));
 
-
+#ifndef P2P_GO_CHANSPEC_BW_20
 	if (chan->band == IEEE80211_BAND_5GHZ) {
 		param.band = WLC_BAND_5G;
 		err = wldev_iovar_getbuf(dev, "bw_cap", &param, sizeof(param),
@@ -4707,6 +4709,10 @@ wl_cfg80211_set_channel(struct wiphy *wiphy, struct net_device *dev,
 
 	} else if (chan->band == IEEE80211_BAND_2GHZ)
 		bw = WL_CHANSPEC_BW_20;
+#else
+	bw = WL_CHANSPEC_BW_20;
+#endif
+
 set_channel:
 	chspec = wf_channel2chspec(_chan, bw);
 	if (wf_chspec_valid(chspec)) {
