323f594c803260beb43380ac723f3d0a48c8df73
Pavan Kunapuli <pkunapuli@nvidia.com>
2013-10-24 19:03:45 +0530

    mmc: host: sdhci: sdio delayed clock gate
    Aggressive clock gate degrades sdio performance.
    Hence, sdio clock gate is delayed.
     - sdio clock gate is done if no further
       transaction starts within 20msec interval
    
    bug 1382490
    bug 1299485
    
    Change-Id: I1b312af39ed6dd9de859536f616498fcd8d5d958
    Signed-off-by: Pavan Kunapuli <pkunapuli@nvidia.com>
    Signed-off-by: Bitan Biswas <bbiswas@nvidia.com>
    Reviewed-on: http://git-master/r/304696
    Reviewed-by: Riham Haidar <rhaidar@nvidia.com>
    Tested-by: Riham Haidar <rhaidar@nvidia.com>

--- drivers/mmc/host/sdhci-tegra.c
+++ drivers/mmc/host/sdhci-tegra.c
@@ -1402,6 +1402,7 @@ static void tegra_sdhci_set_clock(struct sdhci_host *sdhci, unsigned int clock)
 		pm_runtime_put_sync(&pdev->dev);
 		tegra_host->clk_enabled = false;
 	}
+	sdhci->is_clk_on = tegra_host->clk_enabled;
 
 	if (!tegra_host->edp_cap_enabled)
 		return;
@@ -3182,6 +3183,10 @@ static int __devinit sdhci_tegra_probe(struct platform_device *pdev)
 	}
 
 	host = sdhci_pltfm_init(pdev, soc_data->pdata);
+
+	/* sdio delayed clock gate quirk in sdhci_host used */
+	host->quirks2 |= SDHCI_QUIRK2_SDIO_DELAYED_CLK_GATE;
+
 	if (IS_ERR(host))
 		return PTR_ERR(host);
 
@@ -3394,6 +3399,7 @@ static int __devinit sdhci_tegra_probe(struct platform_device *pdev)
 	}
 	pltfm_host->priv = tegra_host;
 	tegra_host->clk_enabled = true;
+	host->is_clk_on = tegra_host->clk_enabled;
 	tegra_host->max_clk_limit = plat->max_clk_limit;
 	tegra_host->ddr_clk_limit = plat->ddr_clk_limit;
 	tegra_host->instance = pdev->id;
@@ -3491,6 +3497,8 @@ static int __devinit sdhci_tegra_probe(struct platform_device *pdev)
 	INIT_DELAYED_WORK(&tegra_host->dw, edp_work_handler);
 
 	device_create_file(&pdev->dev, &dev_attr_cmd_state);
+
+	INIT_DELAYED_WORK(&host->delayed_clk_gate_wrk, delayed_clk_gate_cb);
 
 	if (gpio_is_valid(plat->cd_gpio)) {
 		rc = request_threaded_irq(gpio_to_irq(plat->cd_gpio), NULL,
