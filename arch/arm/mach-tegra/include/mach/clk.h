7ce3d022160a826caac6dbe7306bc858db601f71
Alex Frid <afrid@nvidia.com>
2013-12-20 22:01:00 -0800

    ARM: tegra: dvfs: Add interface to set fmax at vmin
    Added interface to specify clock fmax/vmin limits at run-time. Calling
    this interface updates frequencies in the respective DVFS table to be
    consistent with the new limits (voltage ladder is preserved):
    
    - for voltages below new vmin, the respective frequencies are shifted
    below new fmax to the levels already present in the table; if the 1st
    table entry has frequency above new fmax, all entries below vmin
    are filled in with 1kHz (min rate used in DVFS tables).
    
    - for voltages above new vmin, the respective frequencies are set at
    or above new fmax (not necessarily present in DVFS table before)
    
    - if new vmin is already in the table the respective frequency is set
    to new fmax (not necessarily present in DVFS table before)
    
    Since, such update may result in changing voltage requirement up at
    the same clock frequency, the interface can be called only for clocks
    that are allowed to override core voltage (SDMMC on tegra platforms),
    and only if core voltage is already overridden to the level higher
    than new vmin.
    
    Bug 1423425
    Bug 1423423
    
    Change-Id: I4f61ea6e3f8b6792ed058509339e16bff1947104
    Signed-off-by: Alex Frid <afrid@nvidia.com>
    Reviewed-on: http://git-master/r/348351
    (cherry-picked from commit 1dc582f4488beeaea7e859496034a25300b1cb0b)
    Reviewed-on: http://git-master/r/360675
    Reviewed-by: Venu Byravarasu <vbyravarasu@nvidia.com>
    Tested-by: Bitan Biswas <bbiswas@nvidia.com>

--- arch/arm/mach-tegra/include/mach/clk.h
+++ arch/arm/mach-tegra/include/mach/clk.h
@@ -6,7 +6,7 @@
  * Author:
  *	Erik Gilling <konkers@google.com>
  *
- * Copyright (C) 2010-2012 NVIDIA Corporation
+ * Copyright (C) 2010-2014, NVIDIA CORPORATION.  All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -41,6 +41,7 @@ void tegra_periph_reset_assert(struct clk *c);
 
 int tegra_dvfs_set_rate(struct clk *c, unsigned long rate);
 int tegra_dvfs_override_core_voltage(struct clk *c, int override_mv);
+int tegra_dvfs_set_fmax_at_vmin(struct clk *c, unsigned long f_max, int v_min);
 unsigned long clk_get_rate_all_locked(struct clk *c);
 
 #ifdef CONFIG_ARCH_TEGRA_2x_SOC
