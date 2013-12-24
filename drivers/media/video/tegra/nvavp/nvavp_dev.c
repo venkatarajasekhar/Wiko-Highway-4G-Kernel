8677105a3acee0172817052e842c3abd0eb05d07
Vandana Salve <vsalve@nvidia.com>
2013-12-24 14:28:42 +0530

    video: tegra: nvavp: copy syncpt to userapp
    The possibility of crash because of invalid pointer
    may be due the user process got terminated and driver
    is accessing pointer in non-existent address space.
    Using local variable to ensure access to valid process context.
    
    bug 1419977
    
    Change-Id: Iecee8d769f59016ab4609db531fcc299b65c7689
    Signed-off-by: Vandana Salve <vsalve@nvidia.com>
    Reviewed-on: http://git-master/r/349341
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    Tested-by: Bharat Nihalani <bnihalani@nvidia.com>

--- drivers/media/video/tegra/nvavp/nvavp_dev.c
+++ drivers/media/video/tegra/nvavp/nvavp_dev.c
@@ -1,7 +1,7 @@
 /*
  * drivers/media/video/tegra/nvavp/nvavp_dev.c
  *
- * Copyright (c) 2011-2013, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2011-2014, NVIDIA CORPORATION.  All rights reserved.
  *
  * This file is licensed under the terms of the GNU General Public License
  * version 2. This program is licensed "as is" without any warranty of any
@@ -1229,8 +1229,6 @@ static int nvavp_pushbuffer_submit_ioctl(struct file *filp, unsigned int cmd,
 	int ret = 0, i;
 	unsigned long phys_addr;
 	unsigned long virt_addr;
-	struct nvavp_pushbuffer_submit_hdr *user_hdr =
-			(struct nvavp_pushbuffer_submit_hdr *) arg;
 	struct nvavp_syncpt syncpt;
 
 	syncpt.id = NVSYNCPT_INVALID;
@@ -1310,7 +1308,7 @@ static int nvavp_pushbuffer_submit_ioctl(struct file *filp, unsigned int cmd,
 					      (hdr.flags & NVAVP_UCODE_EXT),
 						clientctx->channel_id);
 
-		if (copy_to_user((void __user *)user_hdr->syncpt, &syncpt,
+		if (copy_to_user((void __user *)hdr.syncpt, &syncpt,
 				sizeof(struct nvavp_syncpt))) {
 			ret = -EFAULT;
 			goto err_reloc_info;
