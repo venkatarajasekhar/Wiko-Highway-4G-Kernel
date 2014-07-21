7de950676d1486ffc3330315045bdb85041b5f0a
ljs <jiansheng.liu@tinnomobile.com>
2014-07-21 20:53:02 +0800

    Modify sharp lcd esd

--- drivers/video/tegra/dc/dsi.c
+++ drivers/video/tegra/dc/dsi.c
@@ -177,12 +177,16 @@ static void lcd_te_work_func(struct work_struct *work)
 		bl = this_dc->out->sd_settings->bl_device;
 		brightness = bl->props.brightness;
 		bl->props.brightness = 0;
+#ifdef CONFIG_MACH_S8515
 		backlight_update_status(bl);
+#endif
 		tegra_dc_disable(this_dc);
 
 		tegra_dc_enable(this_dc);
 		bl->props.brightness = brightness;
+#ifdef CONFIG_MACH_S8515
 		backlight_update_status(bl);
+#endif
 
 		/* start timer again */
 		enable_irq(gpio_to_irq(TE_PIN_GPIO));
